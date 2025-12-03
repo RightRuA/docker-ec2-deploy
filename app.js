bc6569da24194a1d194abfc1d95ada1e36bf9235
RightRuA <mental0421@gmail.com>
2025-12-03 11:37:50 +0900

    Change header paragraph text
    Updated the paragraph content in the header.

--- app.js
+++ app.js
@@ -265,7 +265,7 @@ app.get('/', (req, res) => {
       <div class="container">
         <div class="header">
           <h1>🚀 ${APP_NAME}</h1>
-          <p>웹 애플리케이션 - TodoList, 연락처 폼</p>
+          <p20210954 서원호</p>
         </div>
 
         <div class="tabs">
